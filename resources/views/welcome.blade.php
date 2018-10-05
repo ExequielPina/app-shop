04e8ad8a31a9be5ccf8d9b278eb6571126654a1f
Exequiel <exequiel_pina@hotmail.es>
2018-10-05 17:45:28 +0200

    Edito welcome, inyecto imágenes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('title', 'Bienvenido a App Shop')
+
 @section('body-class', 'landing-page')
 
 @section('content')
@@ -7,11 +9,11 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <h1 class="title">Write the best title for your page.</h1>
+                <h1 class="title">Bienvenido a App Shop.</h1>
                 <h4>Every landing page needs a small description after the big bold title, that's why we added this text here. Add here all the information that can make you or your product create the first impression.</h4>
                 <br />
                 <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="btn btn-danger btn-raised btn-lg">
-                    <i class="fa fa-play"></i> Watch video
+                    <i class="fa fa-play"></i> Conócenos más 
                 </a>
             </div>
         </div>
@@ -23,7 +25,7 @@
         <div class="section text-center section-landing">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
-                    <h2 class="title">Let's talk product</h2>
+                    <h2 class="title">¿Por qué App Shop?</h2>
                     <h5 class="description">This is the paragraph where you can write more details about your product. Keep you user engaged by providing meaningful information. Remember that by this time, the user is curious, otherwise he wouldn't scroll to get here. Add a button if you want the user to see more.</h5>
                 </div>
             </div>
@@ -35,14 +37,14 @@
                             <div class="icon icon-primary">
                                 <i class="material-icons">chat</i>
                             </div>
-                            <h4 class="info-title">First Feature</h4>
+                            <h4 class="info-title">Atendemos tus dudas</h4>
                             <p>Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough.</p>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="info">
                             <div class="icon icon-success">
-                                <i class="material-icons">verified_user</i>
+                                <i class="material-icons">Pago seguro</i>
                             </div>
                             <h4 class="info-title">Second Feature</h4>
                             <p>Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough.</p>
@@ -69,8 +71,8 @@
                     @foreach ($products as $product)
                     <div class="col-md-4">
                         <div class="team-player">
-                            <img src="../assets/img/avatar.jpg" alt="Thumbnail Image" class="img-raised img-circle">
-                            <h4 class="title">{{ $product->name }}<br />
+                            <img src="{{ $product->images()->first()->image }}" alt="Thumbnail Image" class="img-raised img-circle">
+                            <h4 class="title">{{ $product->category->name }}<br/>
                                 <small class="text-muted">Model</small>
                             </h4>
                             <p class="description">{{ $product->description }}</p>
@@ -89,26 +91,26 @@
         <div class="section landing-section">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
-                    <h2 class="text-center title">Work with us</h2>
-                    <h4 class="text-center description">Divide details about your product or agency work into parts. Write a few lines about each one and contact us about any further collaboration. We will responde get back to you in a couple of hours.</h4>
+                    <h2 class="text-center title">Formulario de contacto</h2>
+                    <h4 class="text-center description">Contacta con nosotros.</h4>
                     <form class="contact-form">
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group label-floating">
-                                    <label class="control-label">Your Name</label>
+                                    <label class="control-label">Tu nombre</label>
                                     <input type="email" class="form-control">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group label-floating">
-                                    <label class="control-label">Your Email</label>
+                                    <label class="control-label">Tu correo electrónico</label>
                                     <input type="email" class="form-control">
                                 </div>
                             </div>
                         </div>
 
                         <div class="form-group label-floating">
-                            <label class="control-label">Your Messge</label>
+                            <label class="control-label">Tu mensaje</label>
                             <textarea class="form-control" rows="4"></textarea>
                         </div>
 
