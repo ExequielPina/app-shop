2cfb50b8536abc4ee460b3384d23fd8492f84f0b
Exequiel <exequiel_pina@hotmail.es>
2018-10-05 12:15:03 +0200

    Relación  categoria\producto\imagen

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    // $category->products (Relación entre categoría y  producto)
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    // $product->category (relación entre producto y categoría)
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // $product->productImage (relación entre producto e imagen)
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
 }
